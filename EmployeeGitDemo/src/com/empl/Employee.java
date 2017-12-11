66221c33126bedde559159be8dbc144ef00cd017
KA376091 <KA376091@D-113074638.wipro.com>
2017-12-11 15:27:50 +0530

    inital checkin for sample git

---
+++ EmployeeGitDemo/src/com/empl/Employee.java
@@ -0,0 +1,11 @@
+package com.empl;
+
+public class Employee {
+
+	public static void main(String[] args) {
+		System.out.println("Employee Details:");
+		System.out.println("Employee Id:");
+		System.out.println("Employee Company:");
+		System.out.println("Employee Salary:");
+	}
+}
